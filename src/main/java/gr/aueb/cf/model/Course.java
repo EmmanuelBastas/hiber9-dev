eae0cb0594080c8dc4f4049d39c1c0c988fde470
Emmanuel Christos Bastas <manos.bastas.ma@gmail.com>
2026-05-31 23:22:33 +0300

    fix: add tostring to Course

--- src/main/java/gr/aueb/cf/model/Course.java
+++ src/main/java/gr/aueb/cf/model/Course.java
@@ -54,4 +54,10 @@ public class Course {
 //    public void setTeacher(Teacher teacher) {
 //        this.teacher = teacher;
 //    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%d %s", id, title);
+    }
 }
