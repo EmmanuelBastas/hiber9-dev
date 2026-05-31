ff8e6352fee5207896ae4371c84ee2f2e9d52009
Emmanuel Christos Bastas <manos.bastas.ma@gmail.com>
2026-05-31 23:23:20 +0300

    fix: add JPQL and Criteria API queries

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -5,6 +5,12 @@ import gr.aueb.cf.model.Teacher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
 
 public class App {
 
@@ -13,14 +19,61 @@ public class App {
 
     public static void main(String[] args) {
 
-        Teacher alice = new Teacher("Alice", "W.");
-        Course java = new Course("Java");
-        alice.addCourse(java);
+//        Teacher alice = new Teacher("Alice", "W.");
+//        Course java = new Course("Java");
+//        alice.addCourse(java);
 
         em.getTransaction().begin();
 
-//        em.persist(alice);
-//        em.persist(java);
+        // JPQL
+
+        // Select all teachers
+        String jpql = "SELECT t FROM Teacher t";
+        List<Teacher> teachers = em.createQuery(jpql, Teacher.class).getResultList();
+        teachers.forEach(System.out::println);
+
+        // Select courses of teacher 'Μόσχος'
+        // SQL Injection Attack
+//        String jpql2 = "SELECT c FROM Course c WHERE c.teacher.lastname = 'Μόσχος'";
+//        List<Teacher> courses = em.createQuery(jpql2, Teacher.class).getResultList();
+//        courses.forEach(System.out::println);
+
+        // SQL Injection-free
+//        String jpql3 = "SELECT c FROM Course c WHERE c.teacher.lastname = :lastname";
+//        List<Teacher> courses2 = em.createQuery(jpql3, Teacher.class)
+//                .setParameter("lastname", "Μόσχος")
+//                .getResultList();
+//        courses2.forEach(System.out::println);
+
+        // Criteria API
+
+        // Get all courses
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Course> criteriaQuery = cb.createQuery(Course.class);
+//        Root<Course> root = criteriaQuery.from(Course.class);
+//        criteriaQuery.select(root);
+//
+//        List<Course> courses3 = em.createQuery(criteriaQuery).getResultList();
+//        courses3.forEach(System.out::println);
+
+        // Select all teachers with lastname 'Καπέτης'
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> criteriaQuery = cb.createQuery(Teacher.class);
+//        Root<Teacher> teacherRoot = criteriaQuery.from(Teacher.class);
+//        criteriaQuery.select(teacherRoot).where(cb.equal(teacherRoot.get("lastname"), "Καπέτης"));  // SQL Injection
+//        List<Teacher> teachers4 = em.createQuery(criteriaQuery).getResultList();
+//        teachers4.forEach(System.out::println);
+
+        // SQL Injection-free
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Teacher> criteriaQuery = cb.createQuery(Teacher.class);
+        Root<Teacher> teacherRoot = criteriaQuery.from(Teacher.class);
+        ParameterExpression<String> lastname = cb.parameter(String.class);
+        criteriaQuery.select(teacherRoot).where(cb.equal(teacherRoot.get("lastname"), lastname));  // SQL Injection
+        List<Teacher> teachers4 = em.createQuery(criteriaQuery)
+                .setParameter(lastname, "Καπέτης")
+                .getResultList();
+        teachers4.forEach(System.out::println);
 
         em.getTransaction().commit();
 
@@ -29,3 +82,10 @@ public class App {
 
     }
 }
+
+
+//        em.persist(alice);
+//        em.persist(java);
+//
+//        Teacher alice = em.find(Teacher.class, 1);
+//        alice.setLastname("Wonderland");
